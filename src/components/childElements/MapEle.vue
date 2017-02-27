84efffb271dec0edc49994ccdc9de325c5213f40
张烜久 <369786460@qq.com>
2017-02-27 17:10:59 +0800

    test

--- src/components/childElements/MapEle.vue
+++ src/components/childElements/MapEle.vue
@@ -14,8 +14,7 @@ export default {
   mounted(){
     this.loadBmap().then((BMap) => {
       this.loadElementSuccess();
-      const { properties } = this.eleData;
-      const { currentCity } = properties;
+      const { properties: { currentCity, currentAddress} } = this.eleData;
       
       this.mapInstance = new BMap.Map(this.eleData.id + '');
       // 禁用地图拖拽
@@ -24,7 +23,26 @@ export default {
       // this.mapInstance.enableDoubleClickZoom();
       // // 禁用双指操作缩放
       // this.mapInstance.disablePinchToZoom();
-      this.mapInstance.centerAndZoom(new BMap.Point(currentCity.center.lng, currentCity.center.lat), currentCity.level);
+      const BMapPoint = new BMap.Point(currentCity.center.lng, currentCity.center.lat);
+
+      this.mapInstance.clearOverlays();
+      if(currentAddress && currentAddress.title){
+        const opts = {
+          position : BMapPoint,    // 指定文本标注所在的地理位置
+          offset   : new BMap.Size(10, -30)    //设置文本偏移量
+        }; 
+        const label = new BMap.Label(currentAddress.title, opts);
+        this.mapInstance.addOverlay(label);
+      }
+      const opts2 = {
+          position : new BMap.Point(currentCity.center.lng, currentCity.center.lat),    // 指定文本标注所在的地理位置
+          offset   : new BMap.Size(10, -30)    //设置文本偏移量
+        }; 
+      const label2 = new BMap.Label('hahahah', opts2);
+      this.mapInstance.centerAndZoom(BMapPoint, currentCity.level);
+      this.mapInstance.addOverlay(label2);
+      var marker = new BMap.Marker(BMapPoint);
+      this.mapInstance.addOverlay(marker);
     });
   },
   methods: {
