baf7aa5f09d640e2602083da4a2fa92bf979af16
张烜久 <369786460@qq.com>
2017-04-12 17:05:15 +0800

    input元素选中事件

--- src/components/childElements/InputEle.vue
+++ src/components/childElements/InputEle.vue
@@ -5,13 +5,38 @@
       v-model.trim="eleData.properties.data"
       maxlength="200"
       style="font-size: 100%"
-      :style="eleData.properties.style"
+      @focus="handleFocus"
+      @blur="handleBlur"
+      :style="inputStyle"
       :required="eleData.properties.required" />
   </div>
 </template>
 
 <script>
 export default {
-  props: ['eleData']
+  props: ['eleData'],
+  data() {
+    return {
+      focused: false
+    }
+  },
+  methods: {
+    handleFocus() {
+      this.focused = true;
+    },
+    handleBlur() {
+      this.focused = false;
+    }
+  },
+  computed: {
+    inputStyle() {
+      if(this.focused){
+        console.log('this.focused!');
+        return this.eleData.properties.style;
+      }else{
+        return this.eleData.properties.style;
+      }
+    }
+  }
 }
 </script>
